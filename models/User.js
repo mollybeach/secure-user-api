cdb3595a511f1c82de7e52b4372ee583d83934ae
mollybeach <mollyjbeach@gmail.com>
2024-10-29 12:46:06 -0700

    feat: Add User model with email and password fields
    - Created `User` model using Sequelize's `Model` and `DataTypes`.
    - Defined `email` field with `STRING` type, non-null constraint, unique constraint, and email format validation.
    - Defined `password` field with `STRING` type and non-null constraint.
    - Set model options including the table name (`users`) and model name (`User`).

---
+++ models/User.js
@@ -0,0 +1,27 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users'
+    }
+  );
+  return User;
+};
