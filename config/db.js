64c6ee37c0c0df802ed5add1f9fef87897b1d00a
mollybeach <mollyjbeach@gmail.com>
2024-10-29 18:03:50 -0700

    refactor: Use environment variables for PostgreSQL connection
    - Replaced hardcoded PostgreSQL connection details (user, host, database) with environment variables (DB_USER, DB_HOST, DB_NAME) for better flexibility and security.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'mollybeach',
-  host: 'localhost',
-  database: 'mollybeach',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
 });
